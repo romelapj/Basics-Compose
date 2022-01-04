8c43f3d02e0965977cbfc1fbbc808c9217084ee4
Romel Palomino <romel.palomino@ARFVFGG2SVQ05P.local>
2022-01-04 11:18:32 -0500

    first commit

---
+++ app/src/main/java/com/romelapj/basicscompose/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.romelapj.basicscompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.romelapj.basicscompose.ui.theme.BasicsComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BasicsComposeTheme {
+        Greeting("Android")
+    }
+}
